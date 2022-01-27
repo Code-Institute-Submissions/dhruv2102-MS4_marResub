f6ed5075dd086ece0fcc045d21c45ea19cff9b02
dhruv2102 <kumardh@tcd.ie>
2022-01-27 07:56:55 +0000

    Added some variables to context processor

--- bag/context.py
+++ bag/context.py
@@ -1,4 +1,16 @@
+from decimal import Decimal
+
 
 def bag_contents(request):
-    context = {}
+    bag_items = []
+    total = 0
+    product_count = 0
+    
+    
+    context = {
+        'bag_items': bag_items,
+        'total': total,
+        'product_count': product_count,
+    }
+
     return context
