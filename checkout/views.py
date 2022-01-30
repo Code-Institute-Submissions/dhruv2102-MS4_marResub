e92f70609c62b424c70804f1fa74a55cabe99d83
dhruv2102 <kumardh@tcd.ie>
2022-01-30 20:16:55 +0000

    Added checkout success

--- checkout/views.py
+++ checkout/views.py
@@ -85,18 +85,21 @@ def checkout(request):
 
 
 def checkout_success(request, order_number):
-    save_info = request.session.get('save_info')
+    save_info = request.session.get("save_info")
     order = get_object_or_404(Order, order_number=order_number)
-    messages.success(request, f'Order successfully processed! \
+    messages.success(
+        request,
+        f"Order successfully processed! \
         Your order number is {order_number}. A confirmation \
-        email will be sent to {order.email}.')
+        email will be sent to {order.email}.",
+    )
 
-    if 'bag' in request.session:
-        del request.session['bag']
+    if "bag" in request.session:
+        del request.session["bag"]
 
-    template = 'checkout/checkout_success.html'
+    template = "checkout/checkout_success.html"
     context = {
-        'order': order,
+        "order": order,
     }
 
     return render(request, template, context)
