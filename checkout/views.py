d448bd85c237d9d4eeca572d7c6cd52973c28886
dhruv2102 <kumardh@tcd.ie>
2022-01-30 19:12:44 +0000

    Ran black

--- checkout/views.py
+++ checkout/views.py
@@ -12,13 +12,13 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag', {})
+    bag = request.session.get("bag", {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
-        return redirect(reverse('products'))
+        return redirect(reverse("products"))
 
     current_bag = bag_contents(request)
-    total = current_bag['grand_total']
+    total = current_bag["grand_total"]
     stripe_total = round(total * 100)
     stripe.api_key = stripe_secret_key
     intent = stripe.PaymentIntent.create(
@@ -28,11 +28,11 @@ def checkout(request):
 
     order_form = OrderForm()
 
-    template = 'checkout/checkout.html'
+    template = "checkout/checkout.html"
     context = {
-        'order_form': order_form,
-        'stripe_public_key': stripe_public_key,
-        'client_secret': intent.client_secret,
+        "order_form": order_form,
+        "stripe_public_key": stripe_public_key,
+        "client_secret": intent.client_secret,
     }
 
     return render(request, template, context)
