294415182f9cd241e42a4a5853e2a9a2cbefdbcc
dhruv2102 <kumardh@tcd.ie>
2022-01-30 15:32:05 +0000

    Revert "Added payment intent and ran black"
    This reverts commit d6702f8d315c928b9bce22e1b8a9a99371af5984.

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.checkout, name="checkout")]
+urlpatterns = [
+    path("", views.checkout, name="checkout")
+]
--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
-from django.conf import settings
+
 from .forms import OrderForm
 from bag.context import bag_contents
 
@@ -8,26 +8,21 @@ import stripe
 
 # Create your views here.
 def checkout(request):
-    stripe_public_key = settings.STRIPE_PUBLIC_KEY
-    stripe_secret_key = settings.STRIPE_SECRET_KEY
-
-    bag = request.session.get("bag", {})
+    bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag")
-        return redirect(reverse("products"))
-
+        return redirect(reverse('products'))
+    
     current_bag = bag_contents(request)
-    total = current_bag["grand_total"]
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent(amount=stripe_total)
+    total = current_bag['grand_total']
+    stripe_total = round(toal * 100)
 
     order_form = OrderForm()
-    template = "checkout/checkout.html"
+    template = 'checkout/checkout.html'
     context = {
-        "order_form": order_form,
-        "stripe_public_key": stripe_public_key,
-        "client_secret": intent.client_secret,
+        'order_form': order_form,
+        'stripe_public_key': 'test public',
+        'client_secret': 'test secret',
     }
 
     return render(request, template, context)
