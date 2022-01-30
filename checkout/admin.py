68f2dadc1f947de04d4e47e71925411c0a4b3e8d
dhruv2102 <kumardh@tcd.ie>
2022-01-30 10:45:41 +0000

    Added signals for orders

--- checkout/admin.py
+++ checkout/admin.py
@@ -13,7 +13,6 @@ class OrderAdmin(admin.ModelAdmin):
     readonly_fields = (
         "order_number",
         "date",
-        "delivery_cost",
         "order_total",
         "grand_total",
     )
