89526b25a250c2e9e8f5c7b963dd9d939500c830
dhruv2102 <kumardh@tcd.ie>
2022-01-30 13:30:13 +0000

    Added basic stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'test public',
+        'client_secret': 'test secret',
     }
 
     return render(request, template, context)
