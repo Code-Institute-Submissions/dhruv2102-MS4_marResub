f59339521a319bc8d69e0767c0c389d04767a4f2
dhruv2102 <kumardh@tcd.ie>
2022-02-03 15:06:17 +0000

    Removed secret key and debug

--- fitness_app/settings.py
+++ fitness_app/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-52!j-4+l63j$7e35*n3#l$#6j*0#bsn&*7@yg&lo+l_*bz&i-f"
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPEMENT' in os.environ
 
 ALLOWED_HOSTS = ['localhost', 'ms4-fitness-app']
 
