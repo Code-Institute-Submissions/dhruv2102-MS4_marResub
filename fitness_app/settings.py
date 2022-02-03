c115919f13c7de548121e916b112f1e7c7c6c096
dhruv2102 <kumardh@tcd.ie>
2022-02-03 12:35:14 +0000

    Made postgres database on heroku

--- fitness_app/settings.py
+++ fitness_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,12 +116,19 @@ WSGI_APPLICATION = "fitness_app.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# Local testing and all
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+
+
+# For deploying to heroku
+# DATABASES = {
+#     "default": dj_database_url.parse(os.getenv("DATABASE_URL"))
+# }
 
 
 # Password validation
