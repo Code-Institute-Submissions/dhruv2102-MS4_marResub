16b5e3fd1566c346e73fefd6c01e6e85c10ae992
dhruv2102 <kumardh@tcd.ie>
2022-02-04 11:19:41 +0000

    Added testimonial to admin

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Testimonials
 
 # Register your models here.
+class TestimonialAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'testimony',
+    )
+
+
+admin.site.register(Testimonials, TestimonialAdmin)
--- about/views.py
+++ about/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Testimonials
 # Create your views here.
 
 
@@ -8,3 +8,14 @@ def about(request):
     Returns about page
     """
     return render(request, "about/about.html")
+
+
+def get_testimonial(request):
+    """
+    View to get testimonial
+    """
+    testimony = Testimonials.objects.all()
+    context = {
+        "testimony": testimony
+    }
+    return render(request, 'about/about.html', context)
