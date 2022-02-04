2efa33e3e725f05b5f9b813663d6f0b7069c39b8
dhruv2102 <kumardh@tcd.ie>
2022-02-04 15:52:36 +0000

    Added allowed hosts

--- fitness_app/settings.py
+++ fitness_app/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 DEBUG = 'DEVELOPEMENT' in os.environ
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'ms4-fitness-app.herokuapp.com']
 
 
 # Application definition
